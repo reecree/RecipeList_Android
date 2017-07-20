e6795c0b68775c985a4f11acf40eaf7617e9976e
reecree <ruehschr@gmail.com>
2017-07-20 12:43:11 -0500

    Initial commit

---
+++ app/src/main/java/com/rupert/recipelist/MainActivity.java
@@ -0,0 +1,76 @@
+package com.rupert.recipelist;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.pinterest.android.pdk.PDKCallback;
+import com.pinterest.android.pdk.PDKClient;
+import com.pinterest.android.pdk.PDKException;
+import com.pinterest.android.pdk.PDKResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+        private PDKClient pdkClient;
+        Button loginButton;
+        private final String APP_ID = "4912766377932110327";
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+        PDKClient.configureInstance(this, APP_ID);
+        PDKClient.getInstance().onConnect(this);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loginButton = (Button) findViewById(R.id.login);
+        loginButton.setOnClickListener(this);
+        pdkClient = PDKClient.configureInstance(this, APP_ID);
+        pdkClient.onConnect(this);
+        pdkClient.setDebugMode(true);
+    }
+
+
+    private void onLogin() {
+        List scopes = new ArrayList<String>();
+        scopes.add(PDKClient.PDKCLIENT_PERMISSION_READ_PUBLIC);
+        scopes.add(PDKClient.PDKCLIENT_PERMISSION_WRITE_PUBLIC);
+        scopes.add(PDKClient.PDKCLIENT_PERMISSION_READ_RELATIONSHIPS);
+        scopes.add(PDKClient.PDKCLIENT_PERMISSION_WRITE_RELATIONSHIPS);
+
+        pdkClient.login(this, scopes, new PDKCallback() {
+            @Override
+            public void onSuccess(PDKResponse response) {
+                Log.d(getClass().getName(), response.getData().toString());
+                onLoginSuccess();
+            }
+
+            @Override
+            public void onFailure(PDKException exception) {
+                Log.e(getClass().getName(), exception.getDetailMessage());
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        int vid = v.getId();
+        switch (vid) {
+            case R.id.login:
+                onLogin();
+                break;
+        }
+    }
+
+    private void onLoginSuccess() {
+        Intent i = new Intent(this, HomeActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(i);
+        finish();
+    }
+}
